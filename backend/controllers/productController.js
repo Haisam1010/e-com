d75587a83179a8f37d700f6bee29c9f43aca13fa
Haisam <mohamedhaisam199@gmail.com>
2023-12-18 17:15:02 +0530

    created product controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+import asyncHandler from "../middleware/asyncHandler";
+import Product from "../models/productModel";
+
+
+const getProducts = asyncHandler(async(req,res)=> {
+    const products = await Product.find({});
+    res.json(products);
+})
+const getProductById = asyncHandler(async(req,res)=> {
+    const {id} = req.params
+    const product = await Product.findById(id);
+    
+    if(product){
+       return res.json(product)}
+        else{
+            res.status(404)
+            throw new Error('Product not found')
+        }
+})
+
+export {getProducts, getProductById}
