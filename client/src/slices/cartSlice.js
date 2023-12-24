e5f2c2706bdafaaca61dd47d53d512784f1522b0
Haisam <mohamedhaisam199@gmail.com>
2023-12-24 11:22:38 +0530

    Added Cart Redux

---
+++ client/src/slices/cartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialstate = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : { cartItems: [] };
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        cartItems: [],
+    },
+    reducers: {
+        addToCart(state, action) {
+            const item = action.payload;
+            const existItem = state.cartItems.find((x) => x.product === item.product);
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) =>
+                    x.product === existItem.product ? item : x
+                );
+            } else {
+                state.cartItems.push(item);
+            }
+        },
+        removeFromCart(state, action) {
+            state.cartItems = state.cartItems.filter(
+                (x) => x.product !== action.payload
+            );
+        },
+    },
+
+})
+
+export default cartSlice.reducer;
